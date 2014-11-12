7813b9b97acf9681de35e3aac4ea73e864d45ab3
Kleber Soares <kleberr@msn.com>
2014-11-12 09:43:15 -0300

    #4686352 - Correcao do filtro dos documento no tramite #1

---
+++ protocolle/current_user.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from threading import local
+
+_user = local()
+
+
+class CurrentUserMiddleware(object):
+    def process_request(self, request):
+        _user.value = request.user
+
+
+def get_current_user():
+    return _user.value
--- protocolle/protocolo/autocomplete_light_registry.py
+++ protocolle/protocolo/autocomplete_light_registry.py
@@ -4,27 +4,58 @@ import autocomplete_light
 
 from django.db.models import Q
 
-from models import Documento
+from protocolle.current_user import get_current_user
+
+from protocolle.auxiliar.models import Instituicao_User
+from protocolle.protocolo.models import Documento, Tramite, Tramite_Documento
 
 
 class DocumentoAutocomplete(autocomplete_light.AutocompleteModelBase):
-    search_fields = ('protocolo', 'tipo_documento__nome', 'numero', 
-                     'data_documento', 'data_validade', 'folhas', 
-                     'carater__nome', 'natureza__nome', 'origem__nome', 
-                     'destino__nome', 'assunto', 'interessado__nome', 
+    search_fields = ('protocolo', 'tipo_documento__nome', 'numero',
+                     'data_documento', 'data_validade', 'folhas',
+                     'carater__nome', 'natureza__nome', 'origem__nome',
+                     'destino__nome', 'assunto', 'interessado__nome',
                      'observacao', 'status__nome',)
     autocomplete_js_attributes = {'placeholder': 'Buscar documento...',
-                                  'minimum_characters': 1,}
-    widget_js_attributes = {'max_values': 10,}
+                                  'minimum_characters': 1}
+    widget_js_attributes = {'max_values': 10}
 
     def choices_for_request(self):
-        # if not self.request.user.is_staff:
-        self.choices = Documento.objects.exclude(Q(status__nome='Arquivado') |\
-                                                 Q(status__nome='Entregue'))
+        iu = Instituicao_User.objects.get(user=get_current_user)
+
+        try:
+            # busca os tramites relacionados a insituicao do usuario
+            t = Tramite.objects.filter(
+                Q(origem_id=iu.instituicao) | Q(destino_id=iu.instituicao)
+                ).order_by('-id')[:1]
+            # busca os documentos relacionados aos tramites
+            td = Tramite_Documento.objects.filter(tramite=t).order_by('-id')
+
+            # return qs.filter(
+            #     Q(status__nome='Arquivado') |
+            #     Q(status__nome='Entregue'))
+            self.choices = Documento.objects.filter(
+                Q(status__nome='Tramitando') |
+                Q(status__nome='Parado') |
+                Q(origem_id=iu.instituicao) |
+                Q(destino_id=iu.instituicao) |
+                Q(pk__in=td.all().values('protocolo_id')))
+            # if not self.request.user.is_staff:
+            # self.choices = Documento.objects.exclude(
+            #     Q(status__nome='Arquivado') |
+            #     Q(status__nome='Entregue'))
+        except:
+            # return qs.filter(
+            #     Q(origem_id=iu.instituicao) |
+            #     Q(destino_id=iu.instituicao))
+            self.choices = Documento.objects.filter(
+                Q(status__nome='Tramitando') |
+                Q(status__nome='Parado') |
+                Q(origem_id=iu.instituicao) |
+                Q(destino_id=iu.instituicao))
 
         # self.choices = self.choices.filter(private=False)
         return super(DocumentoAutocomplete, self).choices_for_request()
 
 
 autocomplete_light.register(Documento, DocumentoAutocomplete)
-
