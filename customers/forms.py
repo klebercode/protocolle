7b48999bb3dd6d5f9909564c491609b0fcfa57e7
Kleber Soares <kleberr@msn.com>
2015-02-21 20:55:41 -0300

    Automatizando mais o customers #2

--- customers/forms.py
+++ customers/forms.py
@@ -5,12 +5,26 @@ from django.utils.translation import ugettext_lazy as _
 from protocolle.current_user import get_current_user
 
 
+def get_username():
+    try:
+        return get_current_user().username
+    except:
+        return ''
+
+
+def get_email():
+    try:
+        return get_current_user().email
+    except:
+        return ''
+
+
 class CreateAdminForm(forms.Form):
     _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
     username = forms.CharField(label=_(u'Usuário'), widget=forms.TextInput(
-                               attrs={'value': get_current_user().username}))
+                               attrs={'value': get_username()}))
     email = forms.EmailField(label=_(u'Email'), widget=forms.TextInput(
-                             attrs={'value': get_current_user().email}))
+                             attrs={'value': get_email()}))
     password = forms.CharField(label=_(u'Senha'), widget=forms.PasswordInput())
 
     # TODO: estudar mais sobre isso
