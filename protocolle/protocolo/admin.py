b3d4624ee0a225bb5a9f8fa5fc1a279c5fed16df
Kleber Soares <kleberr@msn.com>
2016-07-11 17:18:22 -0300

    Adiciona action para atualizar os protocolos com ano errado #13

--- protocolle/protocolo/admin.py
+++ protocolle/protocolo/admin.py
@@ -43,9 +43,9 @@ def atualiza_protocolo_num(self, request, queryset):
     """docstring for atualiza_protocolo_num"""
     # if request.user.is_superuser:
     for obj in queryset:
-        # obj.protocolo = obj.protocolo.replace('2015', '2016')
+        obj.documento = obj.documento.replace('2015', '2016')
         # obj.save()
-        queryset.update(documento=obj.documento.replace('2016', '2015'))
+        # queryset.update(documento=obj.documento.replace('2016', '2015'))
 atualiza_protocolo_num.short_description = 'Atualiza Número do Protocolo'
 
 
